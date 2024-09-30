ad47e674d7023d80ee7b277eeb8743d65ad02f42
j-mahapatra <jagannathm.official@gmail.com>
2024-09-30 23:51:57 +0530

    feat: add clerk auth

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SignIn } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className='grid h-screen place-items-center'>
+      <SignIn />
+    </div>
+  );
+}
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SignUp } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className='grid h-screen place-items-center'>
+      <SignUp />
+    </div>
+  );
+}
