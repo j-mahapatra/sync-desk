d8ae791224a939f102d3c9f0fd9c1ed58d22d672
j-mahapatra <jagannathm.official@gmail.com>
2024-10-01 00:49:16 +0530

    feat: create workspace list UI

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import Header from '@/components/Header';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Create Workspace - Sync Desk',
+  description:
+    'Sync Desk is a real-time collaboration tool that uses AI to help distributed teams write, design, and plan together more efficiently.',
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className='flex w-full h-screen flex-col bg-slate-100'>
+      <Header />
+      {children}
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,54 @@
-import Header from '@/components/Header';
-import React from 'react';
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  AlignLeft,
+  CopyPlus,
+  LayoutGrid,
+  Plus,
+  University,
+} from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
 
 export default function Dashboard() {
+  const { user } = useUser();
+  const [userOrganizations, setUserOrganizations] = useState<string[]>([]);
+
   return (
-    <div>
-      <Header />
+    <div className='my-6 p-6 md:px-28 lg:px-32 xl:px-36 space-y-6 '>
+      <div className='flex justify-between'>
+        <h2 className='font-semibold text-3xl'>{`Hello${user?.firstName ? `, ${user.firstName}!` : ''}`}</h2>
+        <Button>
+          <CopyPlus />
+        </Button>
+      </div>
+      <Separator />
+      <div className='flex justify-between'>
+        <h2 className='font-semibold text-2xl text-slate-900'>Workspaces</h2>
+        <div className='flex space-x-2'>
+          <LayoutGrid />
+          <AlignLeft />
+        </div>
+      </div>
+      {userOrganizations.length === 0 && (
+        <div className='flex flex-col w-full justify-center items-center'>
+          <div className='flex flex-col w-fit justify-center items-center border rounded-md p-8 shadow-md space-y-6'>
+            <p className='w-full text-center'>
+              Looks like you do not have any workspaces!
+            </p>
+            <University className='w-36 h-36 text-slate-700' />
+            <Button
+              variant={'outline'}
+              className='flex items-center justify-center'
+            >
+              <Plus className='w-4 h-4 mr-1' />
+              <span>Create a Workspace</span>
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
