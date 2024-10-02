9c3aabb6879ea93b608600d2505c0798e9af6675
j-mahapatra <jagannathm.official@gmail.com>
2024-10-03 03:29:46 +0530

    feat: add dashboard link to logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 export default function Logo() {
   return (
     <div className='flex items-center justify-center'>
-      <Image src={'/logo.png'} alt={'logo'} width={200} height={60} />
+      <Link href='/dashboard'>
+        <Image src={'/logo.png'} alt={'logo'} width={200} height={60} />
+      </Link>
     </div>
   );
 }
