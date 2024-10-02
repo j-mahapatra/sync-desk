9df16b7c4561bb8da76e50f42ae2f48502530072
j-mahapatra <jagannathm.official@gmail.com>
2024-10-03 04:08:35 +0530

    feat: add workspace name in sidebar

--- src/components/DocumentSidebar.tsx
+++ src/components/DocumentSidebar.tsx
@@ -11,10 +11,18 @@ import {
   Plus,
   SmilePlus,
 } from 'lucide-react';
-import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
 import { Button } from '@/components/ui/button';
 import { db } from '@/lib/firebase-config';
-import { DocumentsType } from '@/lib/types';
+import { DocumentsType, WorkspaceType } from '@/lib/types';
 import { Skeleton } from '@/components/ui/skeleton';
 import { cn } from '@/lib/utils';
 import {
@@ -42,6 +50,35 @@ export default function DocumentSidebar() {
   const [newDocName, setNewDocName] = useState<string>('');
   const [isLoadingDocs, setIsLoadingDocs] = useState<boolean>(true);
   const [isCreatingDoc, setIsCreatingDoc] = useState<boolean>(false);
+  const [currentWorkspaceDetails, setCurrentWorkspaceDetails] =
+    useState<WorkspaceType | null>(null);
+
+  useEffect(() => {
+    async function fetchWorkspaceDetails() {
+      setIsLoadingDocs(true);
+      try {
+        const docRef = doc(db, 'workspaces', workspaceId as string);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setCurrentWorkspaceDetails({
+            id: docSnap.id,
+            ...(docSnap.data() as Omit<WorkspaceType, 'id'>),
+          });
+        } else {
+          toast.error('No such document!');
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error('Error fetching documents');
+      }
+      setIsLoadingDocs(false);
+    }
+
+    if (workspaceId) {
+      fetchWorkspaceDetails();
+    }
+  }, [workspaceId]);
 
   useEffect(() => {
     async function fetchDocumentsByWorkspaceId() {
@@ -104,7 +141,13 @@ export default function DocumentSidebar() {
   return (
     <aside className='hidden md:flex md:flex-col md:w-72 h-full p-5 shadow-sm'>
       <div className='flex w-full items-center justify-between my-5'>
-        <span className='font-semibold text-lg'>Workspace name</span>
+        {isLoadingDocs || !currentWorkspaceDetails?.name ? (
+          <Skeleton className='h-12 w-full' />
+        ) : (
+          <span className='font-semibold text-lg'>
+            {currentWorkspaceDetails.name}
+          </span>
+        )}
         <Dialog>
           <DialogTrigger asChild>
             <Button className='text-xs py-1 px-2'>
